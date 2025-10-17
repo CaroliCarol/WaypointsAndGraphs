63342f04930b12bc0a0ab4181176ead1125e4183
CaroliCarol <a59626@alunos.ipb.pt>
2025-10-17 10:25:52 +0100

    1. Traversing a Path

--- Assets/FollowingWP.cs
+++ Assets/FollowingWP.cs
@@ -19,6 +19,8 @@ public class FollowingWP : MonoBehaviour
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+
+        Invoke("GoToRuin", 2);
     }
 
     public void GoToHeli()
@@ -34,8 +36,26 @@ public class FollowingWP : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 }
