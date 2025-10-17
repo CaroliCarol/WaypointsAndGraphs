54133b2333a26d8a835eeffc98fb528355e05d13
CaroliCarol <a59626@alunos.ipb.pt>
2025-10-17 10:55:22 +0100

    Fim

--- Assets/FollowingWP.cs
+++ Assets/FollowingWP.cs
@@ -62,7 +62,7 @@ public class FollowingWP : MonoBehaviour
             goal = g.pathList[currentWP].getId().transform;
             Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
             Vector3 direction = lookAtGoal - this.transform.position;
-            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Time.deltaTime * rotSpeed);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction),Time.deltaTime * rotSpeed);
             this.transform.Translate(0, 0, speed * Time.deltaTime);
         }
     }
